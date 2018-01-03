c5a1422d7e9f4f9cc21abd4fa4c9137ec979fadf
Jerome Anderson <jandersonxlr@gmail.com>
2018-01-02 22:25:36 -0500

    setup a little todo/reminder script

---
+++ extras/my_todo.sh
@@ -0,0 +1,6 @@
+echo "--------------------"
+echo "        TODO        ";
+echo "--------------------"
+echo "Still need to finish neovim plugins";
+echo "also need to finish colors/theme for zsh";
+echo "work on customizing tmux";
--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -12,4 +12,6 @@ fi
 echo "Updating Configuration..."
 (cd ~/.dotfiles && git pull && git submodule update --init --recursive)
 
+# Calls a simple todo file to remind me of what I was doing
+source $HOME/.dotfiles/extras/my_todo.sh
 source $HOME/.dotfiles/zsh/zshrc.sh
