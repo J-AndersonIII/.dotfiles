194effa406d903c902a6db5c7003e8ea06f64527
MrAndersonIII <jandersonxlr@gmail.com>
2018-03-24 16:18:01 -0400

    tmux keybindings

--- extras/my_todo.sh
+++
@@ -1,6 +0,0 @@
-echo "--------------------"
-echo "        TODO        ";
-echo "--------------------"
-echo "Still need to finish neovim plugins";
-echo "also need to finish colors/theme for zsh";
-echo "work on customizing tmux";
